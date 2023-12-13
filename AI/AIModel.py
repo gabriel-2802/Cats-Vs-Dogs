c8e51a3c4cfca1a1ec4cc93a7ee8be3a5d60bf7d
mariosam23 <sampetrumario11@gmail.com>
2023-12-13 16:58:55 +0200

    Added class AI_model

--- AI/AIModel.py
+++ AI/AIModel.py
@@ -1,1 +1,45 @@
-# class AIModel
+import os
+
+from PhotoEval import *
+
+
+class AI_model:
+    def __init__(self):
+        self.optimal_theta = None
+        self.k = 40
+        self.epsilon = 1e-2
+        self.max_steps = 2000
+        self.lmb = 40
+
+    def train(self, data):
+        cols_x = np.size(data['X'], 1)
+        theta1 = initialize_weights(self.epsilon, self.k, cols_x + 1)
+        theta2 = initialize_weights(self.epsilon, 1, self.k + 1)
+        initial_theta = np.concatenate((theta1.flatten(), theta2.flatten()))
+        cf = lambda theta: cost_function(theta, data['X'], data['y'], self.lmb)
+        optimal_theta, cost, num_iterations = fmincg(cf, initial_theta,
+                                                     {'MaxIter': self.max_steps})
+
+        self.optimal_theta = optimal_theta
+
+    def accuracy(self, data, cv_data):
+        pred = forward_progation(data['X'], self.optimal_theta)
+        pred = np.round(pred)
+        accuracy = 1 - np.mean(np.abs(pred - data['y']))
+        print("Using our custom AI model: ")
+        print("Accuracy on training set: ", accuracy)
+
+        pred_cv = forward_progation(cv_data['X'], self.optimal_theta)
+        pred_cv = np.round(pred_cv)
+        accuracy_cv = 1 - np.mean(np.abs(pred_cv - cv_data['y']))
+        print("Accuracy on CV set: ", accuracy_cv)
+        print("\n")
+
+    def save_optimal_theta(self):
+        np.savez("optimal_theta.npz", optimal_theta=self.optimal_theta)
+
+    def predict_photo(self, path):
+        print("Predicting photos in the directory: ", path)
+        for file in os.listdir(path):
+            full_path = os.path.join(path, file)
+            predict_photo(full_path)
--- main.py
+++ main.py
@@ -1,47 +1,10 @@
 from sklearn.linear_model import LogisticRegression
+import os
 
+from AI.AIModel import AI_model
 from PhotoEval import *
 
-def create_model():
-    data = np.load("DataExtract/train_data.npz")
-    cv_data = np.load("DataExtract/CV_data.npz")
-
-    rows_x = np.size(data['X'], 0)
-    cols_x = np.size(data['X'], 1)
-    rows_cv = np.size(cv_data['X'], 0)
-    cols_cv = np.size(cv_data['X'], 1)
-
-    #  k = neurons in hidden layer
-    k = 40
-    # epsilon - chosen to initialise weights
-    epsilon = 1e-2
-
-    theta1 = initialize_weights(epsilon, k, cols_x + 1)
-    theta2 = initialize_weights(epsilon, 1, k + 1)
-
-    initial_theta = np.concatenate((theta1.flatten(), theta2.flatten()))
-    # we have over 30k parameters in our network, therefore we need a large number of steps
-    max_steps = 2000
-    lmb = 40
-
-    # create a lambda function to pass to the minimization function
-    cf = lambda theta: cost_function(theta, data['X'], data['y'], lmb)
-    optimal_theta, cost, num_iterations = fmincg(cf, initial_theta, {'MaxIter': max_steps})
-
-    pred = forward_progation(data['X'], optimal_theta)
-    pred = np.round(pred)
-    accuracy = 1 - np.mean(np.abs(pred - data['y']))
-    print("Using our custom AI model: ")
-    print("Accuracy on training set: ", accuracy)
-
-    pred_cv = forward_progation(cv_data['X'], optimal_theta)
-    pred_cv = np.round(pred_cv)
-    accuracy_cv = 1 - np.mean(np.abs(pred_cv - cv_data['y']))
-    print("Accuracy on CV set: ", accuracy_cv)
-    print("\n")
-
-    np.savez("optimal_theta.npz", optimal_theta=optimal_theta)
-
+def official_AI_model(data, cv_data):
     print("Using an AI model from the library sklearn: ")
     model = LogisticRegression(max_iter=1000)
     model.fit(data['X'], data['y'])
@@ -49,16 +12,20 @@ def create_model():
     print("Accuracy on CV set: ", model.score(cv_data['X'], cv_data['y']))
     print("\n")
 
-import os
-from PhotoEval import predict_photo
-def demo():
-    path = "ourTest"
+def main():
+    AI = AI_model()
+    data = np.load("DataExtract/train_data.npz")
+    cv_data = np.load("DataExtract/CV_data.npz")
+    AI.train(data)
+    AI.accuracy(data, cv_data)
 
-    print("Predicting photos in the directory: ", path)
-    for file in os.listdir(path):
-        full_path = os.path.join(path, file)
-        predict_photo(full_path)
+    official_AI_model(data, cv_data)
+
+    AI.save_optimal_theta()
+
+    # You can add the path to your own directory here
+    PATH = "ourTest"
+    AI.predict_photo(PATH)
 
 if __name__ == "__main__":
-    create_model()
-    demo()
+    main()
